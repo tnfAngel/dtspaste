974f8ab78bf23506b66f09cc04f08acfb64660b2
AlexxTek <xomega820@gmail.com>
2021-01-29 13:23:24 +0100

    update

--- src/tspaste.ts
+++ src/tspaste.ts
@@ -1,28 +1,30 @@
-import Prajax from "https://deno.land/x/cajax@2.0.0/Prajax.js";
+import axiod from "https://deno.land/x/axiod@0.20.0-0/mod.ts";
 const baseURL = "https://jspaste.tnfangel.repl.co";
 
-export class publish {
+export class publicar {
   async publicar(texto: string) {
-    let obtenido = null;
-    await Prajax.post(`${baseURL}/documents`, { texto })
-      .then((res) => {
-        return obtenido = `${baseURL}/${res.key}`;
-      }).catch((result) => {
-        return console.error("[TSPaste Error]: "+result);
+    let respuesta = null;
+    await axiod.post(`${baseURL}/documents`, texto)
+      .then((result) => {
+        return respuesta = `${baseURL}/${result}`;
+      })
+      .catch((error) => {
+        return console.error("[TSPaste Error]: " + error);
       });
-    return this;
+    return respuesta;
   }
 }
 
-export class obtain {
-  async obtener(texto: string) {
-    let obtenido = null;
-    await Prajax.get(`${baseURL}/documents/${texto}`)
-      .then((res) => {
-        return obtenido = res;
-      }).catch((res) => {
-        return console.error("[TSPaste Error]: "+res);
+export class obtener {
+  async obtener(clave: string) {
+    let respuesta = null;
+    await axiod.get(`${baseURL}/documents/${clave}`)
+      .then((result) => {
+        return respuesta = result;
+      })
+      .catch((error) => {
+        return console.error("[TSPaste Error]: " + error);
       });
-    return this;
+    return respuesta;
   }
 }
