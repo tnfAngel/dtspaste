739a4dbd304ab33bce0b703747053fece7efcf42
AlexxTek <xomega820@gmail.com>
2021-01-29 13:53:01 +0100

    hola adios clases :D

--- src/tspaste.ts
+++ src/tspaste.ts
@@ -1,28 +1,26 @@
 import axiod from "https://deno.land/x/axiod@0.20.0-0/mod.ts";
 const baseURL = "https://jspaste.tnfangel.repl.co";
 
-export class tspaste {
-  async publicar(texto: string) {
-    let respuesta = null;
-    await axiod.post(`${baseURL}/documents`, texto)
-      .then((result) => {
-        return respuesta = `${baseURL}/${result}`;
-      })
-      .catch((error) => {
-        return console.error("[TSPaste Error]: " + error);
-      });
-    return respuesta;
-  }
+export async function publicar(texto: string) {
+  let respuesta = null;
+  await axiod.post(`${baseURL}/documents`, texto)
+    .then((result) => {
+      return respuesta = `${baseURL}/${result}`;
+    })
+    .catch((error) => {
+      return console.error("[TSPaste Error]: " + error);
+    });
+  return respuesta;
+}
 
-  async obtener(clave: string) {
-    let respuesta = null;
-    await axiod.get(`${baseURL}/documents/${clave}`)
-      .then((result) => {
-        return respuesta = result;
-      })
-      .catch((error) => {
-        return console.error("[TSPaste Error]: " + error);
-      });
-    return respuesta;
-  }
+export async function obtener(clave: string) {
+  let respuesta = null;
+  await axiod.get(`${baseURL}/documents/${clave}`)
+    .then((result) => {
+      return respuesta = result;
+    })
+    .catch((error) => {
+      return console.error("[TSPaste Error]: " + error);
+    });
+  return respuesta;
 }
