37ea59a0c267a4dd425d456b18662f8a18dc8df7
AlexxTek <xomega820@gmail.com>
2021-01-29 10:38:31 +0100

    update

--- deps.ts
+++ deps.ts
@@ -1,1 +1,1 @@
-export { JqAjaxPostManager, JqAjaxGetManager, JsHelper, UserInteraction } from "https://deno.land/x/jquery_ajax@0.0.1/index.js";
+export { JqAjaxGetManager, JqAjaxPostManager, JsHelper, UserInteraction } from "https://deno.land/x/jquery_ajax@0.0.1/index.js";
--- tspaste.ts
+++ tspaste.ts
@@ -1,47 +1,45 @@
-import { JqAjaxPostManager, JqAjaxGetManager, JsHelper, UserInteraction } from "./deps.ts";
-const baseURL = 'https://jspaste.tnfangel.repl.co'
+import { JqAjaxGetManager, JqAjaxPostManager, JsHelper, UserInteraction } from "./deps.ts";
+const baseURL = "https://jspaste.tnfangel.repl.co";
 
 export class publicar {
-    async publicar(texto: string) {
-        let obtenido = null
-        var userInteraction = new UserInteraction();
-        var jsHelper = new JsHelper();
-        var ajaxPostMgr = new JqAjaxPostManager();
-        ajaxPostMgr.userInteraction = userInteraction;
-        ajaxPostMgr.jsHelper = jsHelper;
-        await ajaxPostMgr.performAjaxPostJson(`${baseURL}/documents`, texto, {
-            successCallback: function (result: { key: any; }) {
-                return obtenido = `${baseURL}/${result.key}`
-            }
-        },
-        {
-            failureCallback: function (textStatus: string, errorThrown: any) {
-            console.log("[jspaste Error]: "+textStatus)
-            }
-        });
-        return this;
-    }
+  async publicar(texto: string) {
+    let obtenido = null;
+    var userInteraction = new UserInteraction();
+    var jsHelper = new JsHelper();
+    var ajaxPostMgr = new JqAjaxPostManager();
+    ajaxPostMgr.userInteraction = userInteraction;
+    ajaxPostMgr.jsHelper = jsHelper;
+    await ajaxPostMgr.performAjaxPostJson(`${baseURL}/documents`, texto, {
+      successCallback: function (result: { key: any }) {
+        return obtenido = `${baseURL}/${result.key}`;
+      },
+    }, {
+      failureCallback: function (textStatus: string, errorThrown: any) {
+        console.log("[jspaste Error]: " + textStatus);
+      },
+    });
+    return this;
+  }
 }
 
 export class obtener {
-    async obtener(a: string) {
-        let obtenido = null
-        const userInteraction = new UserInteraction();
-        var jsHelper = new JsHelper();
-        var ajaxGetMgr = new JqAjaxGetManager();
-        ajaxGetMgr.userInteraction = userInteraction;
-        ajaxGetMgr.jsHelper = jsHelper;
-        ajaxGetMgr.performAjaxGetJson
-        await ajaxGetMgr.performAjaxGetJson(`${baseURL}/documents/${a}`, {
-            successCallback: function (result: any) {
-                obtenido = result;
-            }
-        },
-        {
-            failureCallback: function (textStatus: string, errorThrown: any) {
-                console.log("[jspaste Error]: "+textStatus)
-            }
-        });
-        return this;
-    }
+  async obtener(a: string) {
+    let obtenido = null;
+    const userInteraction = new UserInteraction();
+    var jsHelper = new JsHelper();
+    var ajaxGetMgr = new JqAjaxGetManager();
+    ajaxGetMgr.userInteraction = userInteraction;
+    ajaxGetMgr.jsHelper = jsHelper;
+    ajaxGetMgr.performAjaxGetJson;
+    await ajaxGetMgr.performAjaxGetJson(`${baseURL}/documents/${a}`, {
+      successCallback: function (result: any) {
+        obtenido = result;
+      },
+    }, {
+      failureCallback: function (textStatus: string, errorThrown: any) {
+        console.log("[jspaste Error]: " + textStatus);
+      },
+    });
+    return this;
+  }
 }
